f804c49120a202dcc9c70c988527ee1a2cf29adf
Saotalangim161099 <Saotalangim161099@gmail.com>
2025-05-15 06:46:20 -0400

    style: improve the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,17 +7,18 @@ const Header = () => {
     <header>
       <div className="header-container">
         {/* Profile Image + Name & Title in the Same Row by putting them in profile-section. */}
+        <h1>Phuc Nguyen</h1>
         <div className="profile-section">
           <img src="/Official_selfie.jpg" alt="Phuc Nguyen" className="profile-img" />
           <div className="name-title">
-            <h1>Phuc Nguyen</h1>
+            CS Graduate @ GMU ’25 <br />
             <p className="subtitle">
-              Open to Entry-Level Roles <br />
-              <span className="highlight-role">Software Engineer</span> <br />
-              <span className="highlight-role">Android Developer</span> <br />
-              <span className="highlight-role">Full-stack Developer</span> <br />
-              CS Graduate @ GMU ’25 <br />
-            </p>
+            Open to Entry-Level Roles <br />
+            <span className="highlight-role">Software Engineer</span> <br />
+            <span className="highlight-role">Android Developer</span> <br />
+            <span className="highlight-role">Full-stack Developer</span> <br />
+            
+          </p>
           </div>
         </div>
 
